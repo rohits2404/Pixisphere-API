5a4d384ac41b8445176e9596ef37061ebbe89bad
Rohit Sharma <rohits8731@gmail.com>
2025-06-11 09:01:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-import { create, router as _router, defaults } from 'json-server'
+const jsonServer = require('json-server')
 
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
